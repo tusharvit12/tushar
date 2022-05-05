4252953bd3ff05d7af506aed83a937f74e3d9688
tusharvit12 <tushar.vit12@gmail.com>
2022-05-06 03:22:19 +0530

    Add files via upload
    In Progress

--- JTest.java
+++ JTest.java
@@ -1,7 +1,12 @@
+package jdbcwithframes;
+
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
+import java.time.format.DateTimeFormatter;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -10,119 +15,172 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+@SuppressWarnings("serial")
 public class JTest extends JPanel {
 
     private static JComboBox comboBox;
-    private static JTextField idTextField;
-    private static JTextField nameTextField;
-    private static JTextField emailTextField;
-    private static JTextField addTextField;
-    private static JTextField mobTextField;
-    private static JTextField dobTextField;
+    private static JTextField idTextField = new JTextField(10);
+    private static JTextField nameTextField= new JTextField(10);
+    private static JTextField emailTextField= new JTextField(10);
+    private static JTextField addTextField= new JTextField(10);
+    private static JTextField mobTextField= new JTextField(10);
+    private static JTextField dobTextField= new JTextField(10);
+    
+    private static JLabel idLabel=new JLabel("Enter Primary ID:");
+    private static JLabel nameLabel=new JLabel("Enter Name:");
+    private static JLabel emailLabel=new JLabel("Enter email ID");
+    private static JLabel addLabel=new JLabel("Enter Address");
+    private static JLabel mobLabel=new JLabel("Enter Mobile No");
+    private static JLabel dobLabel=new JLabel("Enter Date of Birth");
     
     
 
     // Create a form with the fields
-    public JTest() {
+    public JTest(JFrame f) {
         super(new BorderLayout());
-        // Panel for the labels
-        JPanel labelPanel = new JPanel(new GridLayout(7, 1)); // 7 rows 1 column
-        add(labelPanel, BorderLayout.WEST);
-
-        // Panel for the fields
-        JPanel fieldPanel = new JPanel(new GridLayout(2, 1)); // 2 rows 1 column
-        add(fieldPanel, BorderLayout.CENTER);
-
-        // Combobox
-        JLabel labelCombo = new JLabel("Select");
+        try {
+        	
         
-        
-
-        // Options in the combobox
-        String[] options = { "Create Record", "Insert Record", "Update Record", "Delete Record"};
-        comboBox = new JComboBox(options);
-        comboBox.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Do something when you select a value
-            	
-            	switch (comboBox.getActionCommand())
-            	{
-            		case "Create Record":
-            		
-            			
-            		case "Insert Record":  idTextField = new JTextField(10);
-                    					   idTextField.setVisible(true);
-                    					  
-                    					   nameTextField = new JTextField(10);
-                    					   nameTextField.setVisible(true);
-                    					 
-                    					   emailTextField = new JTextField(10);
-                    					   emailTextField.setVisible(true);
-                    					   
-                    					   addTextField = new JTextField(10);
-                    					   addTextField.setVisible(true);
-                    					  
-                    					   mobTextField = new JTextField(10);
-                    					   mobTextField.setVisible(true);
-                    					   
-                    					   dobTextField = new JTextField(10);
-                    					   dobTextField.setVisible(true);
-                    					     					   
-                                       	
-            		case "Update Record":
-            			
-            		case "Delete Record":
-            		
-            	}
-
-            }
-        });
-
-        // Textfield
+	        DriverManager.registerDriver(new org.hsqldb.jdbc.JDBCDriver());
+	    	Connection conn = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/xdb", "SA", "");
+	    	PreparedStatement pst = conn.prepareStatement("INSERT INTO BANK_APPLICANT VALUES (?,?,?,?,?,?) ");
+	       
+	        // Panel for the labels
+	        JPanel labelPanel = new JPanel(new GridLayout(2, 1)); // 7 rows 1 column
+	        add(labelPanel, BorderLayout.WEST);
+	
+	        // Panel for the fields
+	        JPanel fieldPanel = new JPanel(new GridLayout(2, 1)); // 2 rows 1 column
+	        add(fieldPanel, BorderLayout.EAST);
+	
+	                // Combobox
+	        JLabel labelCombo = new JLabel("Select");
+	        
+	        // Options in the combobox
+	        String[] options = { "Create Record", "Insert Record", "Update Record", "Delete Record"};
+	        comboBox = new JComboBox(options);
+	        comboBox.setSize(30,10);
+	        fieldPanel.add(comboBox);
+	        
+	        JLabel labelTextField = new JLabel();
+	        
+	        labelPanel.add(labelCombo);
+	        labelPanel.add(labelTextField);
+	        
+	        
+	        //Labels for Table Insert fields
+	       
+	        comboBox.addActionListener(
+	        		new ActionListener()
+	        		{
+	        	
+			            @Override
+			            public void actionPerformed(ActionEvent e) 
+			            {
+			            	System.err.println(comboBox.getSelectedItem());
+			            	switch ((String)comboBox.getSelectedItem())
+			            	{
+			            		case "Create Record":
+			            		
+			            			
+			            		case "Insert Record":  	setPreferredSize(new Dimension(700, 500));
+			            								JPanel newp=new JPanel(new GridLayout(6, 2, 10, 15));
+			            								add(newp, BorderLayout.SOUTH);
+			            								newp.add(idLabel);
+			            								newp.add(idTextField);
+			            								newp.add(nameLabel);
+			                    					    newp.add(nameTextField);
+			                    					    newp.add(emailLabel);
+			                    					    newp.add(emailTextField);
+			                    					    newp.add(addLabel);
+			                    					    newp.add(addTextField);
+			                    					    newp.add(mobLabel);
+			                    					    newp.add(mobTextField);
+			                    					    newp.add(dobLabel);
+			                    					    newp.add(dobTextField);
+			                    					    f.pack();
+			                    					    
+			                    					    JButton insert=new JButton("Insert");
+			                    					    
+			                    				        insert.addActionListener(new ActionListener() 
+			                    				        {	
+			                    				        	 public void actionPerformed(ActionEvent e) {
+			                    					    		
+				                    					    try {
+					                    					    pst.setInt(1, Integer.parseInt(idTextField.getText()));
+					                    						pst.setString(2, nameTextField.getText());
+					                    						pst.setString(3, emailTextField.getText());
+					                    						pst.setString(4, addTextField.getText());
+					                    						pst.setString(5, mobTextField.getText());
+					                    						DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+					                    						java.util.Date date = Date.valueOf(dobTextField.getText());
+					                    						java.sql.Date sqlDate= new java.sql.Date(date.getTime());
+					                    						pst.setDate(6, sqlDate);
+					                    						int row = pst.executeUpdate();
+					                    						pst.close();
+					                    						
+				                    					    }catch(SQLException se) {
+				                    					    	System.out.println(se.getMessage());
+				                    					    	
+				                    					    }catch(NumberFormatException noe) {
+				                    					    	System.out.println(noe.getMessage());
+				                    					    }
+			                    				        }
+			                    				        });
+			                    				        
+			                    					    break;
+			                                       	
+			            		case "Update Record":
+			            			
+			            								break;
+			            			
+			            		case "Delete Record":
+			            		
+			            	}
+			
+			           }
+	        });
+	        
+	        conn.close();
+        }catch(SQLException se) {
+        	
+        }
        
-        JLabel labelTextField = new JLabel();
-        // Add labels
-        labelPanel.add(labelCombo);
-        labelPanel.add(labelTextField);
-
-        // Add fields
-        fieldPanel.add(comboBox);
-        fieldPanel.add(idTextField);
-        fieldPanel.add(nameTextField);
-        fieldPanel.add(emailTextField);
-        fieldPanel.add(addTextField);
-        fieldPanel.add(mobTextField);
-        fieldPanel.add(dobTextField);
-        //fieldPanel.add(textField);
+ 
     }
 
     public static void main(String[] args) {
-        final JTest form = new JTest();
-
-        // Button submit
-        JButton submit = new JButton("Submit Form");
-        submit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-               // createIban((String) comboBox.getSelectedItem(), textField.getText());
-            }
-        });
-
-        // Frame for our test
-        JFrame f = new JFrame("Text Form Example");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.getContentPane().add(form, BorderLayout.NORTH);
-
-        // Panel with the button
-        JPanel p = new JPanel();
-        p.add(submit);
-        f.getContentPane().add(p, BorderLayout.SOUTH);
-
-        // Show the frame
-        f.pack();
-        f.setVisible(true);
+    		
+	    	
+	    	JFrame f = new JFrame("Welcome");
+	        final JTest form = new JTest(f);
+	
+	        // Button submit
+	        JButton submit = new JButton("Submit Form");
+	        submit.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                createIban((String) comboBox.getSelectedItem(), idTextField.getText());
+	            }
+	        });
+	
+	        // Frame for our test
+	       // JFrame f = new JFrame("Welcome");
+	        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        f.getContentPane().add(form, BorderLayout.NORTH);
+	
+	        // Panel with the submit button
+	        JPanel p = new JPanel();
+	        p.add(submit);
+	        f.getContentPane().add(p, BorderLayout.SOUTH);
+	        
+	
+	
+	        // Show the frame
+	       
+	        f.setSize(500, 300);
+	        f.setVisible(true);
+	
     }
 
     private static void createIban(String selectedItem, String text) {
